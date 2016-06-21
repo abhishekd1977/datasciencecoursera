af39d6f9d32105b0556875be1414a0cc5c2ab97f
abhishekd1977 <abhishekd.iitb@gmail.com>
2016-06-21 20:49:23 +0530

    complete.R

--- complete.R
+++ complete.R
@@ -12,3 +12,18 @@ complete <- function(directory, id = 1:332){
     colnames(complete_data_df) <- c("id", "nobs")
     complete_data_df
 }
+
+#######Test Cases
+#> complete("/Users/abhishekdubey/abhishek-git-repos/datasciencecoursera/specdata",1)
+#id nobs
+#1  1  117
+
+##This test case did not pass. The expected output should be ordered as 30, 29....25
+#> complete("/Users/abhishekdubey/abhishek-git-repos/datasciencecoursera/specdata",30:25)
+#id nobs
+#1 25  463
+#2 26  586
+#3 27  338
+#4 28  475
+#5 29  711
+#6 30  932
