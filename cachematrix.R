32f185908114fc2ac06a96e0abbdad458de69d52
abhishekd1977 <abhishekd.iitb@gmail.com>
2016-07-03 17:50:49 +0530

    cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -22,13 +22,32 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
+    ## If the inverse has already been calculated (and the matrix has not changed), 
+    ## then the cachesolve should retrieve the inverse from the cache.
+    ## One way to find out if matrix is not changes is to multiply it by it's inverse
+    ## If the result is an Identity matrix (1,0,0,1), then 
+    
+    ## First create an identity matrix
+    identityMatrix <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
+    ##print(identityMatrix)
+    
     m <- x$getInverse()
+    
     message("before")
     print(m)    
     message("after")
     if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
+        n <- x$get()
+        ##Multiply identyMatrix with its inverse.
+        result <- m %*% n
+        message("result")
+        print(result)
+        
+        if(all.equal(result, identityMatrix)){
+            message("result == identityMatrix")
+            message("getting cached data")
+            return(m)            
+        }
     }
     data <- x$get()
     m <- solve(data, ...)
