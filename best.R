102aee6606493e5dfa28f6f93c4bb75af6500a70
abhishekd1977 <abhishekd.iitb@gmail.com>
2016-07-09 17:39:14 +0530

    best.R

--- best.R
+++ best.R
@@ -3,6 +3,11 @@ best <- function(state, outcome){
     ## Read outcome data
     my_df <- read.csv("outcome-of-care-measures.csv", header = TRUE, na.strings = "Not Available", stringsAsFactors = FALSE)
     outcomes <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
+    state.names <- my_df[, "State"]
+    if (!(outcome %in% names(outcomes)))
+        stop("invalid outcome")
+    if (!(state %in% state.names))
+        stop("invalid state")
     outcome.state <- na.omit(subset(my_df[, c(2,7,outcomes[outcome])], State == state))
     
     ## Check that state and outcome are valid
