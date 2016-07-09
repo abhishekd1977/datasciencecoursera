4758f5049e38fdc56de81d255d9cea4b18c9f3fc
abhishekd1977 <abhishekd.iitb@gmail.com>
2016-07-09 17:53:23 +0530

    best.R

--- best.R
+++ best.R
@@ -4,15 +4,15 @@ best <- function(state, outcome){
     my_df <- read.csv("outcome-of-care-measures.csv", header = TRUE, na.strings = "Not Available", stringsAsFactors = FALSE)
     outcomes <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
     state.names <- my_df[, "State"]
+    
+    ## Check that state and outcome are valid
     if (!(outcome %in% names(outcomes)))
         stop("invalid outcome")
     if (!(state %in% state.names))
         stop("invalid state")
-    outcome.state <- na.omit(subset(my_df[, c(2,7,outcomes[outcome])], State == state))
-    
-    ## Check that state and outcome are valid
     
     ## Return hospital name in that state with lowest 30-day death rate
+    outcome.state <- na.omit(subset(my_df[, c(2,7,outcomes[outcome])], State == state))
     outcome.state.ordered <- outcome.state[order(outcome.state[, 3], outcome.state[, 1]), ]
     outcome.state.ordered[1, "Hospital.Name"]
 }
