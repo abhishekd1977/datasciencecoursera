4fd7782ea421a2f4493d74f5c29b1ef7968a4a95
abhishekd1977 <abhishekd.iitb@gmail.com>
2016-08-28 09:20:52 +0530

    run analysis

--- getting-and-cleaning-data-course-project/run_analysis.R
+++ getting-and-cleaning-data-course-project/run_analysis.R
@@ -6,6 +6,7 @@ features <- read.table(file = "features.txt", header = FALSE, stringsAsFactors =
 ##Read activity labels data
 activity_labels <- read.table(file = "activity_labels.txt", header = FALSE, stringsAsFactors = FALSE)
 
+################# TRAIN DATA PROCESSING- START #################################
 setwd("/Users/abhishekdubey/abhishek-git-repos/datasciencecoursera/UCI_HAR_Dataset/train")
 ##Read X_train data. Assign column names to x_train data from features
 x_train <- read.table(file = "X_train.txt", header = FALSE, stringsAsFactors = FALSE)
@@ -30,5 +31,39 @@ x_train <- x_train[ !duplicated(names(x_train))]
 x_train <- x_train %>% 
     select(matches("std|mean")) %>% 
     cbind(subject_train) %>% 
-    cbind(activity_name = y_train$activity_name) %>%
-    cbind(data_type = c("train"))
+    cbind(activity_name = y_train$activity_name)
+################# TRAIN DATA PROCESSING- END ###################################
+
+################# TEST DATA PROCESSING- START ##################################
+setwd("/Users/abhishekdubey/abhishek-git-repos/datasciencecoursera/UCI_HAR_Dataset/test")
+##Read X_test data. Assign column names to x_test data from features
+x_test <- read.table(file = "X_test.txt", header = FALSE, stringsAsFactors = FALSE)
+colnames(x_test) <- features[,2]
+
+##Read subject_test data. Assign column names to subject_test data
+subject_test <- read.table(file = "subject_test.txt", header = FALSE, stringsAsFactors = FALSE)
+colnames(subject_test) <- c("subject")
+
+##Read y_test(activity) data. Merge y_test data with activity_labels data. Assign column names to merged data.
+y_test <- read.table(file = "y_test.txt", header = FALSE, stringsAsFactors = FALSE)
+y_test <- merge(y_test, activity_labels, by.x = "V1", by.y = "V1")
+colnames(y_test) <- c("activity_id", "activity_name")
+
+##Remove columns with duplicate names. This is needed as dplyr SELECT functions gives ERROR with duplicate columns
+x_test <- x_test[ !duplicated(names(x_test))]
+
+##Select columns with names containing test "mean" or "std". Filter data upfront.
+##Attach subject data
+##Attach activity data
+##Attach a new column data_type with value="train" for all rows.
+x_test <- x_test %>% 
+    select(matches("std|mean")) %>% 
+    cbind(subject_test) %>% 
+    cbind(activity_name = y_test$activity_name)
+
+################# TEST DATA PROCESSING- END ####################################
+
+##Merge train and test data
+merged_data <- rbind(x_train, x_test)
+
+
