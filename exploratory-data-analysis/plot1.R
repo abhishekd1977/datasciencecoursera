894a448094c17ba46ec51d77664a9ce9095876f5
abhishekd1977 <abhishekd.iitb@gmail.com>
2016-09-09 17:33:39 +0530

    plot1

--- exploratory-data-analysis/plot1.R
+++ exploratory-data-analysis/plot1.R
@@ -1,2 +1,3 @@
 setwd("/Users/abhishekdubey/abhishek-git-repos/datasciencecoursera/exploratory-data-analysis")
-mydata <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE, na.strings = "?") %>% filter(Date %in% c("1/2/2007", "2/2/2007"))
+mydata <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE, na.strings = "?") %>% 
+            filter(Date %in% c("1/2/2007", "2/2/2007"))
