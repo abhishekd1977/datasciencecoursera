60bfccc801f0e8c207c56725ba699befff43a796
abhishekd1977 <abhishekd.iitb@gmail.com>
2016-09-14 08:04:13 +0530

    course-project-2 code

--- exploratory-data-analysis/course-project-2/course-project-2.R
+++ exploratory-data-analysis/course-project-2/course-project-2.R
@@ -1,8 +1,16 @@
+## Read file
+library(dplyr)
+setwd("/Users/abhishekdubey/abhishek-git-repos/datasciencecoursera/exploratory-data-analysis/course-project-2")
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
 ## 1. Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
 ## Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
-
-
-
+pm25summary <- select(NEI, year, Emissions) %>% group_by(year) %>% summarize(EmissionsSum = sum(Emissions))
+plot(pm25summary$year, pm25summary$EmissionsSum, type = "l", lwd = 2, xlab = "Year", ylab = "Total Emissions Across All Sources")
+dev.copy(png, file = "plot1.png")
+dev.off()
 
 
 
@@ -23,7 +31,10 @@
 
 
 
+
 ## 5. How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
+
+
 
 
 
